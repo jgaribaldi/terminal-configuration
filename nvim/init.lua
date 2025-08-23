27502dcf8ca9e7e653f39e830f99cb06f13e89e1
jgaribaldi <julita.gari@gmail.com>
2025-08-23 08:31:45 -0300

    Add autoinstall for Python formatters

--- nvim/init.lua
+++ nvim/init.lua
@@ -718,6 +718,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'isort', -- Used to format Python code
+        'black', -- Used to format Python code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
