616f1724a4bd34f1d4c4904154e69bf25b4f5f25
jgaribaldi <julita.gari@gmail.com>
2025-09-21 10:22:11 -0300

    Add suport for Go development

--- nvim/lua/custom/plugins/lsp-plugins.lua
+++ nvim/lua/custom/plugins/lsp-plugins.lua
@@ -175,5 +175,31 @@ return {
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    {
+      'ray-x/go.nvim',
+      dependencies = { -- optional packages
+        'ray-x/guihua.lua',
+        'neovim/nvim-lspconfig',
+        'nvim-treesitter/nvim-treesitter',
+      },
+      opts = {
+        -- lsp_keymaps = false,
+        -- other options
+      },
+      config = function(lp, opts)
+        require('go').setup(opts)
+        local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          pattern = '*.go',
+          callback = function()
+            require('go.format').goimports()
+          end,
+          group = format_sync_grp,
+        })
+      end,
+      event = { 'CmdlineEnter' },
+      ft = { 'go', 'gomod' },
+      build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    },
   },
 }
