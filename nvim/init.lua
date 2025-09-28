1bf3c21f17ed75d24a79ccd21c52e765aac8fe97
jgaribaldi <julita.gari@gmail.com>
2025-09-28 10:22:48 -0300

    Add tabstop configuration for golang

--- nvim/init.lua
+++ nvim/init.lua
@@ -363,3 +363,14 @@ require('ufo').setup {
   end,
 }
 --
+
+-- Tabs in 4 columns for go
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'go',
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = false
+  end,
+})
